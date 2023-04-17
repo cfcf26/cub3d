f06b13789be1c4a881cc01380bb93825ed343549
ekwak <ekwak@student.42seoul.kr>
2023-04-17 19:51:44 +0900

    change fun name

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ekwak <ekwak@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 21:30:45 by ekwak             #+#    #+#             */
-/*   Updated: 2023/04/11 22:31:27 by ekwak            ###   ########.fr       */
+/*   Updated: 2023/04/17 19:49:56 by ekwak            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(int argc, char **argv)
 {
 	parser(argc, argv);
+	init_mlx_struct();
 	test_printf("test");
 	test_print_info();
 	return (0);
